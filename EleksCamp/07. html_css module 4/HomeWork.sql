d24c198e35cda10a5ebd6774d610a658b31deb11
BalchenkoSergiy <sergiybalchenko@gmail.com>
2022-12-13 12:06:20 -0800

    Added 07. html_css module 4

---
+++ EleksCamp/07. html_css module 4/HomeWork.sql
@@ -0,0 +1,3 @@
+* Закінчити верстку CV, зробити PR в репозиторій https://github.com/eleks-bootcamp/cv-project
+* Зверстати карту товару: https://github.com/eleks-bootcamp/online-store
+* Пройти 1-13 рівні в грі https://flexboxfroggy.com/
