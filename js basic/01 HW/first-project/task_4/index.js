aa8cdf4ea26f07fa0f00c95f84b0aa7c5056b7e6
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-02-17 12:04:52 -0600

    Finished JS HomeWork

---
+++ js basic/01 HW/first-project/task_4/index.js
@@ -0,0 +1,6 @@
+prompt('How many 2+2?')
+alert('If your answer 4 - you are right!');
+prompt('What is root from 9?')
+alert('If your answer 3 - you are right!');
+prompt('What is 2 in squar 5?')
+alert('If your answer 32 - you are right!');
