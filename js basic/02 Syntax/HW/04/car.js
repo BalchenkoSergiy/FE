1a4b3a40d721587d69279611e4065fb41b57ebbf
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-02-17 13:49:10 -0600

    Finished task 02/04

---
+++ js basic/02 Syntax/HW/04/car.js
@@ -0,0 +1,23 @@
+// Task 3
+
+const carName = 'AUDI'; 
+let maxCarSpeeed = '280'; 
+let carOwner = 'Serhii'; 
+
+alert(carName)
+alert(maxCarSpeeed)
+alert(carOwner)
+
+maxCarSpeeed = 270;
+console.log('Car Max Speed was changed ', maxCarSpeeed)
+maxCarSpeeed = 260;
+console.log('Car Max Speed was changed ', maxCarSpeeed)
+maxCarSpeeed = 250;
+console.log('Car Max Speed was changed ', maxCarSpeeed)
+
+carOwner = 'Olha'
+console.log('Car owner was changed ', carOwner)
+carOwner = 'Lisa'
+console.log('Car owner was changed ', carOwner)
+carOwner = 'Vova'
+console.log('Car owner was changed ', carOwner)
