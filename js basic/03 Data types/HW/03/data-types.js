2bffdba78dfe6e13c3576283c8438ebf97973907
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-02-25 12:37:52 -0800

    HW 03/03.01

--- js basic/03 Data types/HW/03/data-types.js
+++ js basic/03 Data types/HW/03/data-types.js
@@ -1,19 +1,27 @@
-// Task 02
+// Task 03
 
 let ageOfPerson1 = 18;
 let ageOfPerson2 = '20';
+ageOfPerson2 = 18
+console.log('ageOfPerson2', ageOfPerson2, typeof(ageOfPerson2));
 
 let nameOfAnimal1 = null;
 let nameOfAnimal2 = 'Charlie';
+nameOfAnimal2 = 'Charlie';
+console.log('nameOfAnimal2', nameOfAnimal2, typeof(nameOfAnimal2));
 
 const bestProgramingLanguage1 = 'JavaScript'
 const bestProgramingLanguage2 = 'Java'
 
 let initialValue1;
 let initialValue2 = 0;
+initialValue2 = initialValue1;
+console.log('initialValue2', initialValue2, typeof(initialValue2));
 
 var isJavaScriptProgrammwe1 = true;
 var isJavaScriptProgrammwe2 = false;
 
 let helloText1 = 'Hello!';
 let helloText2 = 'Hello again!';
+helloText2 = 'Hello!';
+console.log('helloText2', helloText2, typeof(helloText2));
