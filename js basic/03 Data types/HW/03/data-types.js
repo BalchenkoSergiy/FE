057808d7045ff9fc38cefaa89fb9edfc12e57c8f
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-02-25 11:36:51 -0800

    Start work on hw 03/03

---
+++ js basic/03 Data types/HW/03/data-types.js
@@ -0,0 +1,19 @@
+// Task 02
+
+let ageOfPerson1 = 18;
+let ageOfPerson2 = '20';
+
+let nameOfAnimal1 = null;
+let nameOfAnimal2 = 'Charlie';
+
+const bestProgramingLanguage1 = 'JavaScript'
+const bestProgramingLanguage2 = 'Java'
+
+let initialValue1;
+let initialValue2 = 0;
+
+var isJavaScriptProgrammwe1 = true;
+var isJavaScriptProgrammwe2 = false;
+
+let helloText1 = 'Hello!';
+let helloText2 = 'Hello again!';
