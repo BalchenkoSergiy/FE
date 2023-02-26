621504a9c250c97340615174118fbbb2ea6c6a4d
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-02-25 22:42:29 -0800

    HW 04/03

---
+++ js basic/04 Data Type conversion/HW/HW 04/03/index.js
@@ -0,0 +1,34 @@
+//  Task 03
+
+console.log('1. Try STRING for console.log --> ', typeof String(console.log), '<-- New value is -->', String(console.log), '<--');
+console.log('1. Try NUMBER for console.log --> ', typeof Number (console.log), '<-- New value is -->', Number (console.log), '<--');
+console.log('1. Try BOOLEAN for console.log --> ', typeof Boolean(console.log), '<-- New value is -->', Boolean(console.log), '<--');
+
+console.log('2. Try STRING for {name:"Maxim"} --> ', typeof String({name:'Maxim'}), '<-- New value is -->', String({name:'Maxim'}), '<--');
+console.log('2. Try NUMBER for {name:"Maxim"} --> ', typeof Number ({name:'Maxim'}), '<-- New value is -->', Number ({name:'Maxim'}), '<--');
+console.log('2. Try BOOLEAN for {name:"Maxim"} --> ', typeof Boolean({name:'Maxim'}), '<-- New value is -->', Boolean({name:'Maxim'}), '<--');
+
+console.log('3. Try STRING for Number --> ', typeof String(Number), '<-- New value is -->', String(Number), '<--');
+console.log('3. Try NUMBER for Number --> ', typeof Number (Number), '<-- New value is -->', Number (Number), '<--');
+console.log('3. Try BOOLEAN for Number --> ', typeof Boolean(Number), '<-- New value is -->', Boolean(Number), '<--');
+
+console.log('4. Try STRING for "" --> ', typeof String(''), '<-- New value is -->', String(''), '<--');
+console.log('4. Try NUMBER for "" --> ', typeof Number (''), '<-- New value is -->', Number (''), '<--');
+console.log('4. Try BOOLEAN for "" --> ', typeof Boolean(''), '<-- New value is -->', Boolean(''), '<--');
+
+console.log('5. Try STRING for 0 --> ', typeof String(0), '<-- New value is -->', String(0), '<--');
+console.log('5. Try NUMBER for 0 --> ', typeof Number (0), '<-- New value is -->', Number (0), '<--');
+console.log('5. Try BOOLEAN for 0 --> ', typeof Boolean(0), '<-- New value is -->', Boolean(0), '<--');
+
+console.log('6. Try STRING for -10 --> ', typeof String(-10), '<-- New value is -->', String(-10), '<--');
+console.log('6. Try NUMBER for -10 --> ', typeof Number (-10), '<-- New value is -->', Number (-10), '<--');
+console.log('6. Try BOOLEAN for -10 --> ', typeof Boolean(-10), '<-- New value is -->', Boolean(-10), '<--');
+
+console.log('7. Try STRING for "-105" --> ', typeof String('-105'), '<-- New value is -->', String('-105'), '<--');
+console.log('7. Try NUMBER for "-105" --> ', typeof Number ('-105'), '<-- New value is -->', Number ('-105'), '<--');
+console.log('7. Try BOOLEAN for "-105" --> ', typeof Boolean('-105'), '<-- New value is -->', Boolean('-105'), '<--');
+
+
+console.log('8. Try STRING for Symbol("key") --> ', typeof String(Symbol('key')), '<-- New value is -->', String(Symbol('key')), '<--');
+console.log('8. Try NUMBER for Symbol("key") --> ', typeof Number (Symbol('key')), '<-- New value is -->', Number (Symbol('key')), '<--');
+console.log('8. Try BOOLEAN for Symbol("key") --> ', typeof Boolean(Symbol('key')), '<-- New value is -->', Boolean(Symbol('key')), '<--');
