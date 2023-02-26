1f317933abcafe23f8d39e3a10a61afb1113ffdf
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-02-25 18:04:35 -0800

    Conversion into Number

--- js basic/04 Data Type conversion/index.js
+++ js basic/04 Data Type conversion/index.js
@@ -5,11 +5,25 @@
             - Неявне
     */
     const age = 20;
-    console.log('Number age:', typeof age)
-    console.log('String age:', typeof String(age))  //  Явна зміна типу даних 
+    console.log('Number age:', typeof age);
+    console.log('String age:', typeof String(age));  //  Явна зміна типу даних 
     
     const updateAge = '1' + 20; // Неявна зміна типу даних
-    console.log('updateAge', typeof updateAge)
+    console.log('updateAge', typeof updateAge);
 
 // Into Number
+    /*
+        Є два шляхи зміни типу даних на STRING:
+            - Явне (Саме цей тип радять використовувати)
+            - Неявне
+    */
+
+    const experienceInJavaScript = '5';
+    console.log('string experienceInJavaScript:', typeof experienceInJavaScript);
+    console.log('Number experienceInJavaScript:', Number (experienceInJavaScript)); //  Явна зміна типу даних 
+    console.log('Type of experienceInJavaScript:', typeof (experienceInJavaScript));
+    
+    console.log('experienceInJavaScript', +experienceInJavaScript); // Неявна зміна типу даних
+    console.log('Type of experienceInJavaScript:', typeof (experienceInJavaScript));
+
 // Into Boolean
