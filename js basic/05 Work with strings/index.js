ead62e7a1ed7e9e63d43d16460adf2514a1a39b6
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-03-04 22:47:06 -0800

    WOrk with Strings - not finished, just save results

---
+++ js basic/05 Work with strings/index.js
@@ -0,0 +1,29 @@
+// Work with STRINGs
+
+const name1 = 'Serhii'
+const name2 = "Serhii"
+const name3 = `Serhii`
+
+console.log(name1)
+console.log(name2)
+console.log(name3)
+
+// Concatenation
+const name = 'Serhii'
+const profession = 'JavaScript - Developer'
+
+const allinfo = name + ' ' + profession
+console.log('AllInfo -->',allinfo)
+
+const allinfo2 = `${name} ${profession}`
+console.log('AllInfo2 -->',allinfo2)
+
+//Length of string
+const programmingLanguage = 'JavaScript'
+console.log('Length of programmingLanguage -->', programmingLanguage.length)
+
+// Getting symbol from string
+const programmingLanguage2 = 'Python'
+console.log('Getting 1 symbol from  programmingLanguage2-->', programmingLanguage2[0])
+
+//-00:14:30
