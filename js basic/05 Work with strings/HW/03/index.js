e441c2097150feab52c709fb3786ba4c6dc3a8d0
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-03-11 13:34:11 -0800

    F_HW 05/03

--- js basic/05 Work with strings/HW/03/index.js
+++ js basic/05 Work with strings/HW/03/index.js
@@ -1,19 +1,5 @@
-// HW 05/02
+// HW 05/03
 
-const myName = 'Serhii'
-const  programmingLanguage = 'JavaScript'
-const  courseCreatorName = 'unknown'
-const  reasonText = 'It`s my dream'
-const  numberOfMonth = 1
+const userAnswer = ((prompt('Insert your Name please')).toUpperCase()).trim();
 
-let myInfoText = `Всем привет! Меня зовут, ${myName}. Сейчас я изучаю язык программирования
-${programmingLanguage.toUpperCase()} на курсе по ${programmingLanguage.toUpperCase()} у ${courseCreatorName}.
-Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а)
-${programmingLanguage.toUpperCase()} ${numberOfMonth} месяцев. Я уверен(а), что пройду данный курс до
-конца!`
-
-console.log(myInfoText)
-console.log('Length -->>',myInfoText.length)
-console.log('First symbol -->>',myInfoText[0])
-
-console.log('Last symbol -->>',myInfoText[myInfoText.length - 1])
+console.log(userAnswer)
