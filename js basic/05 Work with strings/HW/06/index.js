4da04b3ea939a0ae3042360c9aa8c327fb063ed0
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-03-11 14:50:38 -0800

    S_HW 05/06

---
+++ js basic/05 Work with strings/HW/06/index.js
@@ -0,0 +1,14 @@
+// HW 06/05
+
+const userString = (prompt('Insert your text please')).trim();
+let startSliceIndex = (prompt('Insert your startSliceIndex please')).trim();
+startSliceIndex = Number(startSliceIndex)
+
+let endSliceIndex = (prompt('Insert your endSliceIndex please')).trim();
+endSliceIndex = Number(endSliceIndex)
+
+
+console.log(`Origin string: ${userString}`)
+console.log(`Result string: ${userString.slice(startSliceIndex, endSliceIndex)}`)
+
+alert(`Result string: ${userString.slice(startSliceIndex, endSliceIndex)}`)
