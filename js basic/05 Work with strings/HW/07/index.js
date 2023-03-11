72fe6fc5f16b908338c807121e4770f94f266710
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-03-11 15:40:29 -0800

    F_HW 05/07

--- js basic/05 Work with strings/HW/07/index.js
+++ js basic/05 Work with strings/HW/07/index.js
@@ -1,8 +1,22 @@
 // HW 06/05
 
-const userText = (prompt('Insert your text please')).trim();
-const wordFromText = (prompt('Insert word from userText')).trim();
-const indexOfWord = Number (userText.indexOf(wordFromText))
+const  javaScriptDescription = 'JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили. Является реализацией спецификации ECMAScript. JavaScript обычно используется как встраиваемый язык для программного доступа к объектам приложений.'
+const stringCenter = Math.floor(javaScriptDescription.length / 2)
 
-console.log(`Result string: ${userText.slice(0, indexOfWord)}`)
-alert(`Result string: ${userText.slice(0, indexOfWord)}`)
+//4
+console.log(`Result string: ${(javaScriptDescription.slice(0, stringCenter))} + ${(javaScriptDescription.slice(0, stringCenter))}  + ${(javaScriptDescription.slice(0, stringCenter))}`)
+
+//5
+let tempString = (javaScriptDescription.slice(0, stringCenter)) + (javaScriptDescription.slice(0, stringCenter))  + (javaScriptDescription.slice(0, stringCenter))
+const tempStringCenter = Math.floor(tempString.length / 2)
+console.log(`tempStringCenter: ${tempString[tempStringCenter]}`)
+
+//1
+console.log(`Original string: ${javaScriptDescription}`)
+console.log(`Result string: ${javaScriptDescription.slice(0, stringCenter)}`)
+
+//2
+console.log(`Result string: ${(javaScriptDescription.slice(0, stringCenter)).replaceALL('a', 'A')}`)
+
+//3
+console.log(`Result string: ${(javaScriptDescription.slice(0, stringCenter)).replaceALL(' ', '')}`)
