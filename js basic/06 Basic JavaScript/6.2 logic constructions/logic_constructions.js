e2da9bf921df006eed21c026e3291e34d82eba92
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-03-13 16:05:37 -0700

    JS IF

---
+++ js basic/06 Basic JavaScript/6.2 logic constructions/logic_constructions.js
@@ -0,0 +1,33 @@
+// if
+const isFronEndDeveloper = true;
+
+if (isFronEndDeveloper) {
+    console.log('Congratulation! You are FrontEnd Developer!')
+} else {
+    console.log('You are not FrontEnd Developer')
+}
+
+const closingTime = 8;
+const currentTime = 9;
+
+if (currentTime > closingTime) {
+    console.log('Store is closed')
+} else {
+    console.log('Store is open, WELCOME!')
+}
+
+const developerJobType = 'Front-End';
+
+if (developerJobType === 'Front-End') {
+    console.log ('2000$')
+} else if (developerJobType ==='Back-End') {
+    console.log ('1500$')
+} else if (developerJobType ==='Full-Stack') {
+    console.log ('3500$')
+}  else {
+    console.log('Not Accepted')
+}
+
+// if else
+// switch case
+// ? :
