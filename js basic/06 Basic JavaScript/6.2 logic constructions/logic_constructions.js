84a1c225c7739be934fd368f3654db9312b6eb64
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-03-13 16:20:36 -0700

    JS IF Else, Switch case, ? :

--- js basic/06 Basic JavaScript/6.2 logic constructions/logic_constructions.js
+++ js basic/06 Basic JavaScript/6.2 logic constructions/logic_constructions.js
@@ -7,6 +7,7 @@ if (isFronEndDeveloper) {
     console.log('You are not FrontEnd Developer')
 }
 
+// if else
 const closingTime = 8;
 const currentTime = 9;
 
@@ -28,6 +29,27 @@ if (developerJobType === 'Front-End') {
     console.log('Not Accepted')
 }
 
-// if else
+
 // switch case
-// ? :
+
+switch (developerJobType) {
+    case 'Front-End': 
+        console.log ('2000$');
+        break;
+    case 'Back-End': 
+        console.log ('1500$');
+        break;
+    case 'Full-Stack': 
+        console.log ('3500$');
+        break;
+    default:
+        console.log('Not Accepted')
+}
+
+// ? : (По факту це заміна if else)
+const favoriteDrink = 'Coffee'
+const message = favoriteDrink === 'Coffee' 
+    ? 'Your favorite drink is Coffee' 
+    : 'You don`t like coffee'
+
+console.log('message -->>', message)
