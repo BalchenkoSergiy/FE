61147db616b0815001ef3304d54e22222aac638f
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-03-28 20:52:25 -0600

    Add conditions to questions

--- js basic/06 Basic JavaScript/HW/02/hw_06_02.js
+++ js basic/06 Basic JavaScript/HW/02/hw_06_02.js
@@ -1,6 +1,41 @@
 // task 02
 let first_question = prompt('How much is 2+2?')
+
+if (Number(first_question)  === 4) {
+    console.log('Correct!')
+} else {
+    console.log('Incorrect!')
+}
+
 let second_question = prompt('How much is 2*2?')
+
+if (Number(second_question)  === 4) {
+    console.log('Correct!')
+} else {
+    console.log('Incorrect!')
+}
+
 let third_question = prompt('How much is 5-3-1?')
+
+if (Number(third_question)  === 1) {
+    console.log('Correct!')
+} else {
+    console.log('Incorrect!')
+}
+
 let forth_question = prompt('How much is 10-2-1+5?')
+
+if (Number(forth_question)  === 12) {
+    console.log('Correct!')
+} else {
+    console.log('Incorrect!')
+}
+
 let fifth_question = prompt('How much is 2+2*2?')
+
+if (Number(fifth_question)  === 6) {
+    console.log('Correct!')
+} else {
+    console.log('Incorrect!')
+}
+
