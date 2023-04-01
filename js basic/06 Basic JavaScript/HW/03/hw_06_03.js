04bf4bef741e0169f8e779fe2aaa1a406f2d1867
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-03-31 23:27:46 -0700

    f hw 06_03

--- js basic/06 Basic JavaScript/HW/03/hw_06_03.js
+++ js basic/06 Basic JavaScript/HW/03/hw_06_03.js
@@ -1,10 +1,27 @@
 // task 03
-confirm('JavaScript appeared in 1995?')
-confirm('Is the JavaScript specification called ECMAScript?')
-confirm('JavaScript was created in 1 month?')
+let questionOne = confirm('JavaScript appeared in 1995?')
 
+if (questionOne == true) {
+    console.log('Correct!')
+    } else {
+        console.log('Incorrect!')
+        }
 
+let questionTwo = confirm('Is the JavaScript specification called ECMAScript?')
 
+if (questionTwo == true) {
+    console.log('Correct!')
+    } else {
+        console.log('Incorrect!')
+        }
+
+let questionThree = confirm('JavaScript was created in 1 month?')
+
+if (questionThree == true) {
+    console.log('Correct!')
+    } else {
+        console.log('Incorrect!')
+        }
 
 
 
