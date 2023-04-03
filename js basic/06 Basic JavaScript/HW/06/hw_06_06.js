c8309e2c342ab01f0f5f2d39106dd3371d04770e
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-04-02 17:34:01 -0700

    S HW 06_06

---
+++ js basic/06 Basic JavaScript/HW/06/hw_06_06.js
@@ -0,0 +1,113 @@
+// task 05
+
+// Use While
+
+let var_test = 100
+let result = 0
+while (var_test > 0) {
+    result += var_test
+    console.log(`var_test = ${var_test}, result = ${result}`)
+    console.log(`Result = ${result}`)
+    var_test -= 1
+    }
+
+
+
+
+
+
+
+//let questionOne = confirm('JavaScript appeared in 1995?')
+//
+//if (questionOne == true) {
+//    console.log('Correct!')
+//    } else {
+//        console.log('Incorrect!')
+//        }
+//
+//let questionTwo = confirm('Is the JavaScript specification called ECMAScript?')
+//
+//if (questionTwo == true) {
+//    console.log('Correct!')
+//    } else {
+//        console.log('Incorrect!')
+//        }
+//
+//let questionThree = confirm('JavaScript was created in 1 month?')
+//
+//if (questionThree == true) {
+//    console.log('Correct!')
+//    } else {
+//        console.log('Incorrect!')
+//        }
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let correctAnswers = 0
+//let incorrectAnswers = 0
+//
+//let first_question = prompt('How much is 2+2?')
+//
+//if (Number(first_question)  === 4) {
+//    console.log('Correct!')
+//    correctAnswers += 1
+//} else {
+//    console.log('Incorrect!')
+//    incorrectAnswers += 1
+//}
+//
+//let second_question = prompt('How much is 2*2?')
+//
+//if (Number(second_question)  === 4) {
+//    console.log('Correct!')
+//    correctAnswers += 1
+//} else {
+//    console.log('Incorrect!')
+//    incorrectAnswers += 1
+//}
+//
+//let third_question = prompt('How much is 5-3-1?')
+//
+//if (Number(third_question)  === 1) {
+//    console.log('Correct!')
+//    correctAnswers += 1
+//} else {
+//    console.log('Incorrect!')
+//    incorrectAnswers += 1
+//}
+//
+//let forth_question = prompt('How much is 10-2-1+5?')
+//
+//if (Number(forth_question)  === 12) {
+//    console.log('Correct!')
+//    correctAnswers += 1
+//} else {
+//    console.log('Incorrect!')
+//    incorrectAnswers += 1
+//}
+//
+//let fifth_question = prompt('How much is 2+2*2?')
+//
+//if (Number(fifth_question)  === 6) {
+//    console.log('Correct!')
+//    correctAnswers += 1
+//} else {
+//    console.log('Incorrect!')
+//    incorrectAnswers += 1
+//}
+//
+//alert(`Test is finished! Correct answers -> ${correctAnswers}, Incorrect answers -> ${incorrectAnswers}`)
