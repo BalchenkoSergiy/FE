b3b5bbe3a4be124dac717c0599907f63d2a5d8ef
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-04-08 17:40:22 -0600

    added checkymbol for hw 06/08

--- js basic/06 Basic JavaScript/HW/08/hw_06_08.js
+++ js basic/06 Basic JavaScript/HW/08/hw_06_08.js
@@ -1,33 +1,39 @@
 // task 07
 
 const clientName = prompt('Insert Your name please')
-const clientPassword = prompt('Insert Your password please')
+let clientPassword = ''
+let checkLength = false
 
-if (clientPassword != '')
-    {
-    if (clientPassword.length > 3 && clientPassword.length <= 20) 
+    while (checkLength == false)
         {
-            console.log(`Details (Length check):
-            1. clientName: ${clientName}
-            2. clientPassword: ${clientPassword}
-            3. clientPasswordLenght: ${clientPassword.length}`)
+            clientPassword = prompt('Insert Your password please')
+            if (clientPassword.length >= 3 && clientPassword.length <= 20) 
+                {
 
-            alert(`Congratulation ${clientName}, your password length is ${clientPassword.length}%!`)         
-        }
-    else
-        {
-            console.log(`Details (Lengt check):
-            1. clientName: ${clientName}
-            2. clientPassword: ${clientPassword}
-            3. clientPasswordLenght: ${clientPassword.length}`)
+                    console.log(`Details (Length check):
+                    1. clientName: ${clientName}
+                    2. clientPassword: ${clientPassword}
+                    3. clientPasswordLenght: ${clientPassword.length}`)
 
-            alert(`Error! your password length need to have lenth between 3 and 20 symbols!`) 
+                    alert(`Congratulation ${clientName}, your password length is ${clientPassword.length}!`) 
+                    checkLength = true        
+                }
+            else
+                {
+                    console.log(`Details (Lengt check):
+                    1. clientName: ${clientName}
+                    2. clientPassword: ${clientPassword}
+                    3. clientPasswordLenght: ${clientPassword.length}`)
+
+                    alert(`Error! your password length need to have lenth between 3 and 20 symbols!`) 
+                }
         }
 
     let varCoursor = 0
     let varEndPoint = clientPassword.length - 1
+    let checkSymbol = false
 
-    while (varCoursor <= varEndPoint) 
+    while (checkSymbol == false && varCoursor <= varEndPoint) 
         {
             console.log(`Details (CamelCase check):
             1. varCoursor: ${varCoursor}
@@ -36,9 +42,16 @@ if (clientPassword != '')
             4. clientPassword: ${clientPassword}
             5. Check symbol: ${clientPassword[varCoursor]}`)
 
+            if (clientPassword[varCoursor] === clientPassword[varCoursor].toUpperCase()) 
+                {
+                    console.log(`Check symbol: DONE`)
+                    checkSymbol = true
+
+                }
+
             varCoursor += 1
         }
-    }
+    
 
 //    else if (clientSpentForAllTime + orderAmount >= 301 && clientSpentForAllTime + orderAmount <= 500) 
 //        {
