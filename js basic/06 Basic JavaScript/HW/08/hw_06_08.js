9ff416eca36d79b57976135b66fd39581c8749ce
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-04-08 20:00:07 -0600

    hw 08/07 added camel cxase checker

--- js basic/06 Basic JavaScript/HW/08/hw_06_08.js
+++ js basic/06 Basic JavaScript/HW/08/hw_06_08.js
@@ -1,169 +1,66 @@
-// task 07
+// task 08
 
 const clientName = prompt('Insert Your name please')
 let clientPassword = ''
 let checkLength = false
+let checkSymbol = false
+let varCoursor = 0
+let varEndPoint = clientPassword.length - 1
 
-    while (checkLength == false)
-        {
-            clientPassword = prompt('Insert Your password please')
-            if (clientPassword.length >= 3 && clientPassword.length <= 20) 
-                {
+while (checkLength == false)
+    {
+        clientPassword = prompt('Insert Your password please')
+        if (clientPassword.length >= 3 && clientPassword.length <= 20) 
+            {
+                console.log(`Details (Length check):
+                1. clientName: ${clientName}
+                2. clientPassword: ${clientPassword}
+                3. clientPasswordLenght: ${clientPassword.length}`)
+                alert(`Congratulation ${clientName}, your password length is ${clientPassword.length}!`) 
+                checkLength = true        
+            }
+        else
+            {
+                console.log(`Details (Length check):
+                1. clientName: ${clientName}
+                2. clientPassword: ${clientPassword}
+                3. clientPasswordLenght: ${clientPassword.length}`)
+                alert(`Error! your password length need to have lenth between 3 and 20 symbols!`) 
+            }
+    }
+console.log(`Finished check password length`)
+console.log(`************************************************`)
+console.log(`************************************************`)
+console.log(`Start check CamelCase letters`)
 
-                    console.log(`Details (Length check):
-                    1. clientName: ${clientName}
-                    2. clientPassword: ${clientPassword}
-                    3. clientPasswordLenght: ${clientPassword.length}`)
-
-                    alert(`Congratulation ${clientName}, your password length is ${clientPassword.length}!`) 
-                    checkLength = true        
-                }
-            else
-                {
-                    console.log(`Details (Lengt check):
-                    1. clientName: ${clientName}
-                    2. clientPassword: ${clientPassword}
-                    3. clientPasswordLenght: ${clientPassword.length}`)
-
-                    alert(`Error! your password length need to have lenth between 3 and 20 symbols!`) 
-                }
-        }
-
-    let varCoursor = 0
-    let varEndPoint = clientPassword.length - 1
-    let checkSymbol = false
-
-    while (checkSymbol == false && varCoursor <= varEndPoint) 
-        {
-            console.log(`Details (CamelCase check):
-            1. varCoursor: ${varCoursor}
-            2. varEndPoint: ${varEndPoint}
-            3. clientPasswordLenght: ${clientPassword.length}
-            4. clientPassword: ${clientPassword}
-            5. Check symbol: ${clientPassword[varCoursor]}`)
-
-            if (clientPassword[varCoursor] === clientPassword[varCoursor].toUpperCase()) 
-                {
-                    console.log(`Check symbol: DONE`)
-                    checkSymbol = true
-
-                }
-
-            varCoursor += 1
-        }
-    
-
-//    else if (clientSpentForAllTime + orderAmount >= 301 && clientSpentForAllTime + orderAmount <= 500) 
-//        {
-//            discount = 20
-//            alert(`Congratulation ${clientName}, your discount is ${discount}%!`)
-//            orderAmount = (orderAmount / 100) * 80 
-//            alert(`Your orderAmount is ${orderAmount}!`)
-//            clientSpentForAllTime = clientSpentForAllTime + orderAmount
-//            console.log(`${clientName} spent for all time after counting: ${clientSpentForAllTime}`)
-//
-//            console.log(`Details:
-//            1. clientName: ${clientName}
-//            2. orderAmount: ${orderAmount}
-//            3. clientSpentForAllTime: ${clientSpentForAllTime}
-//            4. discount: ${discount}`)
-//        }
-//    else if (clientSpentForAllTime + orderAmount > 500) 
-//        {
-//            discount = 30
-//            alert(`Congratulation ${clientName}, your discount is ${discount}%!`)
-//            orderAmount = (orderAmount / 100) * 70 
-//            alert(`Your orderAmount is ${orderAmount}!`)
-//            clientSpentForAllTime = clientSpentForAllTime + orderAmount
-//            console.log(`${clientName} spent for all time after counting: ${clientSpentForAllTime}`)
-//
-//            console.log(`Details:
-//            1. clientName: ${clientName}
-//            2. orderAmount: ${orderAmount}
-//            3. clientSpentForAllTime: ${clientSpentForAllTime}
-//            4. discount: ${discount}`)
-//        }
-//    else
-//        {
-//            discount = 0
-//
-//            console.log(`Details:
-//            1. clientName: ${clientName}
-//            2. orderAmount: ${orderAmount}
-//            3. clientSpentForAllTime: ${clientSpentForAllTime}
-//            4. discount: ${discount}`)
-//        }
-
-
-//let orderAmount =  Number((prompt(`How much money ${clientName} spent today?`)).trim())
-//console.log(`orderAmount: ${orderAmount}`)
-//console.log(`type of orderAmount: ${typeof(orderAmount)}`)
-//let clientSpentForAllTime = 0
-//let discount = 0
-//console.log(`clientSpentForAllTime before counting: ${clientSpentForAllTime}`)
-//
-//while (isNaN(orderAmount) == true || orderAmount == 0) {
-//    alert(`Hi you need to insert only numbers. Please try againe.`)
-//    orderAmount =  Number((prompt(`How much money ${clientName} spent today?`)).trim())
-//    console.log(`orderAmount: ${orderAmount}`)
-//}
-//
-//
-//if (orderAmount > 0){
-//    if (clientSpentForAllTime + orderAmount >= 100 && clientSpentForAllTime  + orderAmount <= 300) 
-//        {
-//            discount = 10
-//            alert(`Congratulation ${clientName}, your discount is ${discount}%!`)
-//            orderAmount = (orderAmount / 100) * 90 
-//            alert(`Your orderAmount is ${orderAmount}!`)
-//            clientSpentForAllTime = clientSpentForAllTime + orderAmount
-//            console.log(`${clientName} Spent for all time after counting: ${clientSpentForAllTime}`)
-//
-//
-//            console.log(`Details:
-//            1. clientName: ${clientName}
-//            2. orderAmount: ${orderAmount}
-//            3. clientSpentForAllTime: ${clientSpentForAllTime}
-//            4. discount: ${discount}`)
-//        }
-//    else if (clientSpentForAllTime + orderAmount >= 301 && clientSpentForAllTime + orderAmount <= 500) 
-//        {
-//            discount = 20
-//            alert(`Congratulation ${clientName}, your discount is ${discount}%!`)
-//            orderAmount = (orderAmount / 100) * 80 
-//            alert(`Your orderAmount is ${orderAmount}!`)
-//            clientSpentForAllTime = clientSpentForAllTime + orderAmount
-//            console.log(`${clientName} spent for all time after counting: ${clientSpentForAllTime}`)
-//
-//            console.log(`Details:
-//            1. clientName: ${clientName}
-//            2. orderAmount: ${orderAmount}
-//            3. clientSpentForAllTime: ${clientSpentForAllTime}
-//            4. discount: ${discount}`)
-//        }
-//    else if (clientSpentForAllTime + orderAmount > 500) 
-//        {
-//            discount = 30
-//            alert(`Congratulation ${clientName}, your discount is ${discount}%!`)
-//            orderAmount = (orderAmount / 100) * 70 
-//            alert(`Your orderAmount is ${orderAmount}!`)
-//            clientSpentForAllTime = clientSpentForAllTime + orderAmount
-//            console.log(`${clientName} spent for all time after counting: ${clientSpentForAllTime}`)
-//
-//            console.log(`Details:
-//            1. clientName: ${clientName}
-//            2. orderAmount: ${orderAmount}
-//            3. clientSpentForAllTime: ${clientSpentForAllTime}
-//            4. discount: ${discount}`)
-//        }
-//    else
-//        {
-//            discount = 0
-//
-//            console.log(`Details:
-//            1. clientName: ${clientName}
-//            2. orderAmount: ${orderAmount}
-//            3. clientSpentForAllTime: ${clientSpentForAllTime}
-//            4. discount: ${discount}`)
-//        }
-//}
+while (checkSymbol == false && Number(varCoursor) != Number(varEndPoint)) 
+    {
+        console.log(`************************************************`)
+        console.log(`Started check CamelCase letters`)
+        if (clientPassword[varCoursor] === clientPassword[varCoursor].toUpperCase())
+            {
+                console.log(`Symbol is: ${clientPassword[varCoursor]}`)
+                console.log(`Symbol is UpperCase = true`)
+                checkSymbol = true
+            }
+        else
+            {
+                console.log(`Symbol is: ${clientPassword[varCoursor]}`)
+                console.log(`Symbol is UpperCase = false`)
+                console.log(`****************************************`)
+                if ((clientPassword.length - 1) != varCoursor)
+                    {
+                        varCoursor += 1
+                    }
+                else if ((clientPassword.length - 1) == varCoursor)
+                    {
+                        console.log(`No UpperCase in your Password`)
+                        clientPassword = prompt('Insert Your password again please')
+                        varCoursor = 0
+                    }
+                else
+                    {
+                        checkSymbol = true
+                    }
+            } 
+    }
