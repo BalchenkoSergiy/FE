dd5cae275f63ed0a5a81dad902e8f69ea729cf05
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-04-11 11:22:27 -0700

    worked on hw 06/08

--- js basic/06 Basic JavaScript/HW/08/hw_06_08.js
+++ js basic/06 Basic JavaScript/HW/08/hw_06_08.js
@@ -37,124 +37,122 @@ if (clientPassword !== '')
             }
     }
         
-    console.log(`Finished check Length`)
-    console.log(`************************************************`)
-    console.log(`************************************************`)
-    console.log(`Start check Numbers letters`)
+console.log(`Finished check Length`)
+console.log(`checkLength = ${checkLength}`)
+console.log(`************************************************`)
+console.log(`************************************************`)
+console.log(`Start check Numbers letters`)
 
-    //while (Number(varCoursor) != Number(varEndPoint)) 
-    while (varCoursor != varEndPoint) 
-        {
-            console.log(`************************************************`)
-            console.log(`Started check Number letters`)
-            if  (
-                    clientPassword[varCoursor] == 0 || 
-                    clientPassword[varCoursor] == 1 || 
-                    clientPassword[varCoursor] == 2 ||
-                    clientPassword[varCoursor] == 3 || 
-                    clientPassword[varCoursor] == 4 || 
-                    clientPassword[varCoursor] == 5 ||
-                    clientPassword[varCoursor] == 6 ||
-                    clientPassword[varCoursor] == 7 || 
-                    clientPassword[varCoursor] == 8 || 
-                    clientPassword[varCoursor] == 9
-                )
-                {
-                    console.log(`Symbol is: ${clientPassword[varCoursor]}`)
-                    console.log(`Symbol is Number = true`)
-                    checkNumber = true
-                    varCoursor = 0
-                    console.log(`Details (Length check):
-                    1. clientName: ${clientName}
-                    2. clientPassword: ${clientPassword}
-                    3. client symbol: ${clientPassword[varCoursor]}
-                    4. clientPasswordLenght: ${clientPassword.length}`)
-                }
-            else
-                {
-                    console.log(`Symbol is: ${clientPassword[varCoursor]}`)
-                    console.log(`Symbol is Number = false`)
-                    console.log(`****************************************`)
-                    if ((clientPassword.length - 1) != varCoursor)
-                        {
-                            varCoursor += 1
-
-                            console.log(`Details (Length check):
-                            1. clientName: ${clientName}
-                            2. clientPassword: ${clientPassword}
-                            3. client symbol: ${clientPassword[varCoursor]}
-                            4. clientPasswordLenght: ${clientPassword.length}`)
-                        }
-                    else if ((clientPassword.length - 1) == varCoursor)
-                        {
-                            console.log(`No Numbers in your Password`)
-                            varCoursor = 0
-
-                            console.log(`Details (Length check):
-                            1. clientName: ${clientName}
-                            2. clientPassword: ${clientPassword}
-                            3. client symbol: ${clientPassword[varCoursor]}
-                            4. clientPasswordLenght: ${clientPassword.length}`)
-                        }
-                } 
-        }
+//while (Number(varCoursor) != Number(varEndPoint)) 
+while ((clientPassword.length) > varCoursor) 
+    {
+        console.log(`************************************************`)
+        console.log(`Started check Number letters`)
+        if  (
+                clientPassword[varCoursor] == 0 || 
+                clientPassword[varCoursor] == 1 || 
+                clientPassword[varCoursor] == 2 ||
+                clientPassword[varCoursor] == 3 || 
+                clientPassword[varCoursor] == 4 || 
+                clientPassword[varCoursor] == 5 ||
+                clientPassword[varCoursor] == 6 ||
+                clientPassword[varCoursor] == 7 || 
+                clientPassword[varCoursor] == 8 || 
+                clientPassword[varCoursor] == 9
+            )
+            {
+                console.log(`Symbol is: ${clientPassword[varCoursor]}`)
+                console.log(`Symbol is Number = true`)
+                checkNumber = true
+                varCoursor = 0
+                console.log(`Details (Number check):
+                1. clientName: ${clientName}
+                2. clientPassword: ${clientPassword}
+                3. client symbol: ${clientPassword[varCoursor]}
+                4. clientPasswordLenght: ${clientPassword.length}`)
+            }
+        else
+            {
+                console.log(`Symbol is: ${clientPassword[varCoursor]}`)
+                console.log(`Symbol is Number = false`)
+                console.log(`Details (Number check):
+                        1. clientName: ${clientName}
+                        2. clientPassword: ${clientPassword}
+                        3. client symbol: ${clientPassword[varCoursor]}
+                        4. clientPasswordLenght: ${clientPassword.length}`)
+                console.log(`****************************************`)
+                if ((clientPassword.length - 1) != varCoursor)
+                    {
+                        varCoursor += 1
+                    }
+                else if ((clientPassword.length - 1) == varCoursor)
+                    {
+                        console.log(`No Numbers in your Password`)
+                        varCoursor = 0
+                        console.log(`Details (Length check):
+                        1. clientName: ${clientName}
+                        2. clientPassword: ${clientPassword}
+                        3. client symbol: ${clientPassword[varCoursor]}
+                        4. clientPasswordLenght: ${clientPassword.length}`)
+                    }
+            } 
+    }
+    
+console.log(`Finished check Number letters`)
+console.log(`************************************************`)
+console.log(`************************************************`)
+console.log(`Start check CamelCase letters`)
+    
+while ((clientPassword.length-1) > varCoursor) 
+    {   
+        console.log(`************************************************`)
+        console.log(`Started check CamelCase letters`)
+        if (
+                (clientPassword[varCoursor] === clientPassword[varCoursor].toUpperCase()) 
+                &&  
+                (clientPassword[varCoursor] != 0 || 
+                clientPassword[varCoursor] != 1 || 
+                clientPassword[varCoursor] != 2 ||
+                clientPassword[varCoursor] != 3 || 
+                clientPassword[varCoursor] != 4 || 
+                clientPassword[varCoursor] != 5 ||
+                clientPassword[varCoursor] != 6 ||
+                clientPassword[varCoursor] != 7 || 
+                clientPassword[varCoursor] != 8 || 
+                clientPassword[varCoursor] != 9)
+            )
+            {
+                console.log(`Symbol is: ${clientPassword[varCoursor]}`)
+                console.log(`Symbol is UpperCase = true`)
+                checkSymbol = true
+                varCoursor = 0
+            }
+        else
+            {
+                console.log(`Symbol is: ${clientPassword[varCoursor]}`)
+                console.log(`Symbol is UpperCase = false`)
+                console.log(`****************************************`)
+                varCoursor += 1
+                console.log(`Details (Length check):
+                1. clientName: ${clientName}
+                2. clientPassword: ${clientPassword}
+                3. client symbol: ${clientPassword[varCoursor]}
+                4. clientPasswordLenght: ${clientPassword.length}`)
+            }
+                  
+    } 
         
-    console.log(`Finished check Number letters`)
-    console.log(`************************************************`)
-    console.log(`************************************************`)
-    console.log(`Start check CamelCase letters`)
-        
-    while (Number(varCoursor) != Number(varEndPoint)) 
-        {   
-            console.log(`************************************************`)
-            console.log(`Started check CamelCase letters`)
-            if (
-                    (clientPassword[varCoursor] === clientPassword[varCoursor].toUpperCase()) 
-                    &&  
-                    (clientPassword[varCoursor] != 0 || 
-                    clientPassword[varCoursor] != 1 || 
-                    clientPassword[varCoursor] != 2 ||
-                    clientPassword[varCoursor] != 3 || 
-                    clientPassword[varCoursor] != 4 || 
-                    clientPassword[varCoursor] != 5 ||
-                    clientPassword[varCoursor] != 6 ||
-                    clientPassword[varCoursor] != 7 || 
-                    clientPassword[varCoursor] != 8 || 
-                    clientPassword[varCoursor] != 9)
-                )
-                {
-                    console.log(`Symbol is: ${clientPassword[varCoursor]}`)
-                    console.log(`Symbol is UpperCase = true`)
-                    checkSymbol = true
-                    varCoursor = 0
-                }
-            else
-                {
-                    console.log(`Symbol is: ${clientPassword[varCoursor]}`)
-                    console.log(`Symbol is UpperCase = false`)
-                    console.log(`****************************************`)
-
-                    varCoursor += 1
-                    console.log(`Details (Length check):
-                    1. clientName: ${clientName}
-                    2. clientPassword: ${clientPassword}
-                    3. client symbol: ${clientPassword[varCoursor]}
-                    4. clientPasswordLenght: ${clientPassword.length}`)
-                }
-                      
-        } 
-            
-    console.log(`Finished check Camel Case`)
-    console.log(`************************************************`)
-    console.log(`************************************************`)
+console.log(`Finished check Camel Case`)
+console.log(`************************************************`)
+console.log(`************************************************`)
 
 
 
-console.log(`Details (Length check):
+console.log(`Details:
 1. clientName: ${clientName}
 2. clientPassword: ${clientPassword}
 
-Results of cheking of password:
+Results of cheking:
 checkLength = ${checkLength}
 checkSymbol = ${checkSymbol}
 checkNumber = ${checkNumber}`)
