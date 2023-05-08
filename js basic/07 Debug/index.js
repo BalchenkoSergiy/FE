34d4491da2114c7a96eee09820acb5ac58de13b8
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-05-07 19:52:39 -0600

    added breakpoints.js

--- js basic/07 Debug/index.js
+++ js basic/07 Debug/index.js
@@ -1,2 +1,11 @@
 // 07 Debug
+ let sum = 0;
+ console.log('Initial sum ->', sum)
+ const numberOfElements = 10;
+ for (let i=1; i < numberOfElements; i += 1) {
+    console.log('i / sum', i, sum);
+    sum += i;
+ }
 
+const theHalfOfSum = sum / 2;
+console.log('theHalfOfSum', theHalfOfSum);
