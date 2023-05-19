0250108557b15473743ac91c9d94ff3040d9ec57
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-05-19 15:56:41 -0600

    HW JS 07/01 in progress. Wrong condition

--- js basic/07 Debug/HW/01/index.js
+++ js basic/07 Debug/HW/01/index.js
@@ -1,9 +1,14 @@
+// Task 01
 let health = prompt('Введите число параметра "здоровье" для персонажа')
-if () {
-        health < 0 || health!
+
+if ((health <= 0) || (health.type == undefined)) {
+        console.log(`Value: ${health}`)
+        console.log(`Type: ${health.type}`)
         alert('Параметр "здоровье" должен быть больше нуля!')
     } 
-else () 
+else 
     {
-        alert(`Параметр "здоровье" равен ${Health}`);
+        console.log(`Value: ${health}`)
+        console.log(`Type: ${health.type}`)
+        alert(`Параметр "здоровье" равен ${health}`);
     }
