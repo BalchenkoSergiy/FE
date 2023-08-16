03cf14a700b6e4d7835de8d6b19250dd0222928d
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-08-16 12:12:45 -0600

    Added JS_notes

--- js basic/index.js
+++ js basic/index.js
@@ -8,3 +8,10 @@ alert("123")
 */
 
 prompt("What's your sign?", "Hello")
+
+/*
+    • Document: Treats all the HTML documents 
+    • Elements: Tags used inside HTML document 
+    • Text: Content of tags 
+    • Attributes: Here, “href” is an attribute
+*/
