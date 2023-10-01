73e48e7782792ec5fc388ae0559675faa708f403
BalchenkoSergiy <sergiybalchenko@gmail.com>
2023-10-01 12:42:24 -0600

    part 10 comments

---
+++ JavaScript Developer Course/chapter 02/part 10/script.js
@@ -0,0 +1,14 @@
+var name = 'Jack';
+console.log(name);
+
+name = 'Jane';
+console.log(name);
+
+var color;
+console.log(color);
+
+color = 'red';
+console.log(color);
+
+//JavaScript names can include:
+//letters, numbers, $, _
